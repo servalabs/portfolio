a67258ef4cb23707bcb46ee78afd438c41c51a93
PRIYANSHU <priyanshu.uc507@gmail.com>
2025-02-11 18:47:16 +0530

    section addition

--- src/partials/Portfolio/index.tsx
+++ src/partials/Portfolio/index.tsx
@@ -1,90 +1,103 @@
-// Style
-
-import style from './index.module.css'
+import React from 'react';
+import { Trans, useTranslation } from 'react-i18next';
 
 // Components
-import Section from 'components/Section'
-import Container, { Row } from 'components/Container'
-import ContentBlock from 'components/ContentBlock'
-import ImageTrigger from 'components/ImageTrigger'
-import Square from 'components/Square'
-import Heading from 'components/Heading'
-import { List, ListItem } from 'components/List'
+import Section from 'components/Section';
+import Container, { Row } from 'components/Container';
+import ContentBlock from 'components/ContentBlock';
+import ImageTrigger from 'components/ImageTrigger';
+import Heading from 'components/Heading';
 
-// Hooks
-import { Trans, useTranslation } from 'react-i18next'
-// Hooks
+// Styles
+import style from './index.module.css';
 
-function Portfolio() {
-  const { t } = useTranslation('translation', { keyPrefix: 'portfolio' })
-  const intro: string[] = t('intro', { returnObjects: true })
-  const portfolio: string[] = t('portfolio', { returnObjects: true })
+// Types
+interface VideoProps {
+  id: string;
+  src: string;
+}
+
+const VideoPlayer: React.FC<VideoProps> = ({ id, src }) => (
+  <video id={id} autoPlay muted loop playsInline className={style.video}>
+    <source src={src} type="video/mp4" />
+  </video>
+);
+
+const Portfolio: React.FC = () => {
+  const { t } = useTranslation('translation', { keyPrefix: 'portfolio' });
+  const intro: string[] = t('intro', { returnObjects: true });
+  const portfolio: string[] = t('portfolio', { returnObjects: true });
+  const pd: string[] = t('pd', { returnObjects: true });
+
+  const imageTriggerComponents = {
+    ImageBW: <ImageTrigger name="bw" sizes={[2.5, 2.5]} />,
+    ImageTesla: <ImageTrigger name="tesla" sizes={[2, 3]} />,
+    ImageNO1: <ImageTrigger name="no1" />,
+    ImageNO2: <ImageTrigger name="no2" />,
+    ImageNO3: <ImageTrigger name="no3" sizes={[2, 2.5]} />,
+    ImagePow: <ImageTrigger name="pow" sizes={[2, 3]} />
+  };
+
+  const videos: VideoProps[] = [
+    { id: 'skReel', src: '/projects/sk/2.mp4' },
+    { id: 'fbReel', src: '/projects/fb/3.mp4' },
+    { id: 'feudiReel', src: '/projects/feudi/4.mp4' },
+    { id: 'aqReel', src: '/projects/aq/1.mp4' }
+  ];
 
   return (
     <Section name="portfolio" className={style.root}>
+      {/* Intro Section */}
       <Container grid outerRightOnMobile>
         <Row start={1} end={2}>
-          <Heading misaligned key={intro[0]}>
+          <Heading misaligned>
             <>
               <pre className={style.pre}>{intro[0]}</pre>
-              <Trans
-                i18nKey="portfolio.intro.1"
-                components={{
-                  ImageBW: <ImageTrigger name="bw" sizes={[2.5, 2.5]} />,
-                  ImageTesla: <ImageTrigger name="tesla" sizes={[2, 3]} />,
-                  ImageNO1: <ImageTrigger name="no1" />,
-                  ImageNO2: <ImageTrigger name="no2" />,
-                  ImageNO3: <ImageTrigger name="no3" sizes={[2, 2.5]} />
-                }}
-              />
+              <Trans i18nKey="portfolio.intro.1" components={imageTriggerComponents} />
             </>
           </Heading>
         </Row>
       </Container>
-      <Container grid key={intro[2]}>
+
+      <Container grid>
         <Row start={3} end={1}>
           <ContentBlock>
-            <Trans
-              i18nKey="portfolio.intro.2"
-              components={{
-                ImageBW: <ImageTrigger name="bw" sizes={[2.5, 2.5]} />,
-                ImagePow: <ImageTrigger name="pow" sizes={[2, 3]} />
-              }}
-            />
-
+            <Trans i18nKey="portfolio.intro.2" components={imageTriggerComponents} />
             <div>{intro[3]}</div>
           </ContentBlock>
         </Row>
       </Container>
+
+      {/* Portfolio Section */}
       <Container grid outerRightOnMobile className={style.projectSection}>
         <Row start={2} end={2}>
-          <Heading key={portfolio[0]}>{portfolio[0]}</Heading>
+          <Heading>{portfolio[0]}</Heading>
         </Row>
       </Container>
-      <Container grid key={portfolio[1]}>
+
+      <Container grid>
         <Row start={2} end={1}>
           <ContentBlock>
             <div>
-              <Trans
-                i18nKey="portfolio.portfolio.1"
-                components={{
-                  ImageBW: <ImageTrigger name="bw" sizes={[2.5, 2.5]} />,
-                  ImageTesla: <ImageTrigger name="tesla" sizes={[2, 3]} />,
-                  ImageNO1: <ImageTrigger name="no1" />,
-                  ImageNO2: <ImageTrigger name="no2" />,
-                  ImageNO3: <ImageTrigger name="no3" sizes={[2, 2.5]} />
-                }}
-              />
-              <Trans
-                i18nKey="portfolio.portfolio.2"
-                components={{
-                  ImageBW: <ImageTrigger name="bw" sizes={[2.5, 2.5]} />,
-                  ImageTesla: <ImageTrigger name="tesla" sizes={[2, 3]} />,
-                  ImageNO1: <ImageTrigger name="no1" />,
-                  ImageNO2: <ImageTrigger name="no2" />,
-                  ImageNO3: <ImageTrigger name="no3" sizes={[2, 2.5]} />
-                }}
-              />
+              <Trans i18nKey="portfolio.portfolio.1" components={imageTriggerComponents} />
+              <Trans i18nKey="portfolio.portfolio.2" components={imageTriggerComponents} />
+            </div>
+          </ContentBlock>
+        </Row>
+      </Container>
+
+      {/* Product Design Section */}
+      <Container grid outerRightOnMobile className={style.projectSection}>
+        <Row start={2} end={2}>
+          <Heading>{pd[0]}</Heading>
+        </Row>
+      </Container>
+
+      <Container grid>
+        <Row start={2} end={1}>
+          <ContentBlock>
+            <div>
+              <Trans i18nKey="portfolio.pd.1" components={imageTriggerComponents} />
             </div>
           </ContentBlock>
         </Row>
@@ -92,26 +105,11 @@ function Portfolio() {
 
       <div className={style.cardContainer} id="card-container" />
 
-      <video id="skReel" autoPlay muted loop playsInline className={style.video}>
-        <source src="/projects/sk/2.mp4" type="video/mp4" />
-      </video>
-      <video id="fbReel" autoPlay muted loop playsInline className={style.video}>
-        <source src="/projects/fb/3.mp4" type="video/mp4" />
-      </video>
-      <video id="feudiReel" autoPlay muted loop playsInline className={style.video}>
-        <source src="/projects/feudi/4.mp4" type="video/mp4" />
-      </video>
-      <video id="aqReel" autoPlay muted loop playsInline className={style.video}>
-        <source src="/projects/aq/1.mp4" type="video/mp4" />
-      </video>
-      <video id="aqReel" autoPlay muted loop playsInline className={style.video}>
-        <source src="/projects/aq/1.mp4" type="video/mp4" />
-      </video>
-      <video id="aqReel" autoPlay muted loop playsInline className={style.video}>
-        <source src="/projects/aq/1.mp4" type="video/mp4" />
-      </video>
- 
+      {videos.map((video) => (
+        <VideoPlayer key={video.id} {...video} />
+      ))}
     </Section>
-  )
-}
-export default Portfolio
+  );
+};
+
+export default Portfolio;
